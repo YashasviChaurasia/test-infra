c07f89ff918e3563ad13b754ac84370f2a384d84
yashasvi <yashasvi@ibm.com>
2026-08-25 12:55:30 +0530

    fix: restore standalone output for container builds
    Signed-off-by: yashasvi <yashasvi@ibm.com>

--- torchci/next.config.js
+++ torchci/next.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 module.exports = {
+  output: "standalone",
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.modules = [path.resolve(__dirname), "node_modules"];
